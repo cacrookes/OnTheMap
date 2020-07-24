78b8623d030d96d05ee42b960fb0ace5c2635e1a
Christopher Crookes <cacrookes@gmail.com>
2020-07-23 18:42:40 -0700

    Adding StudentInformation struct in StudentInformation.swift.

---
+++ On The Map/Model/StudentInformation.swift
@@ -0,0 +1,24 @@
+//
+//  StudentInformation.swift
+//  On The Map
+//
+//  Created by Christopher Crookes on 2020-07-23.
+//  Copyright © 2020 Christopher Crookes. All rights reserved.
+//
+
+import Foundation
+
+struct StudentInformation: Codable {
+    let firstName: String
+    let lastName: String
+    let longitude: Double
+    let latitude: Double
+    let mapString: String
+    let mediaURL: String
+    let uniqueKey: String
+    let objectId: String
+    let createdAt: String
+    let udpatedAt: String
+}
+
+
