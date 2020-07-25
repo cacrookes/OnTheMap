ecf7a1fc9e5c387c28011940b315e1d57002df88
Christopher Crookes <cacrookes@gmail.com>
2020-07-25 15:53:37 -0700

    Adding View Location View Controller.

---
+++ On The Map/Controllers/ViewLocationViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewLocationViewController.swift
+//  On The Map
+//
+//  Created by Christopher Crookes on 2020-07-25.
+//  Copyright © 2020 Christopher Crookes. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewLocationViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func finishClicked(_ sender: Any) {
+    }
+}
