57f63040fe6a7e3782765c3b69c0a2150037847d
Christopher Crookes <cacrookes@gmail.com>
2020-07-25 22:02:02 -0700

    Adding LoginError enum.

---
+++ On The Map/Model/LoginError.swift
@@ -0,0 +1,13 @@
+//
+//  LoginError.swift
+//  On The Map
+//
+//  Created by Christopher Crookes on 2020-07-25.
+//  Copyright © 2020 Christopher Crookes. All rights reserved.
+//
+
+import Foundation
+
+enum LoginError {
+    case invalidCredentials(String)
+}
